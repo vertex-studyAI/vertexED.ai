64e0557ce404f0081d2006d5ccc8c6ae6f6243fd
vertexed25-byte <vertexed.25@gmail.com>
2025-09-11 16:37:25 +0530

    Update Main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -8,7 +8,7 @@ export default function Main() {
     { title: "Study Zone", to: "/study-zone", info: "All in 1 tool section for your calculators, activity logs and more." },
     { title: "AI Chatbot", to: "/chatbot", info: "Ask questions and get help on general topics or just simply have a discussion on academics." },
     { title: "Study Planner", to: "/planner", info: "Plan sessions and deadlines for your busy schedule in an instant." },
-    { title: "Answer Reviewer", to: "/AnswerReviewer", info: "Not just a basic reviewer; a strict teacher of sorts which also gives the best feedback on how you can improve" },
+    { title: "Answer Reviewer", to: "/answer-reviewer", info: "Not just a basic reviewer; a strict teacher of sorts which also gives the best feedback on how you can improve" },
     { title: "IB/IGCSE Paper Maker", to: "/paper-maker", info: "Create syllabus-aligned papers which are actually helpful." },
     { title: "Note taker + Flashcards + Quiz", to: "/notetaker", span: "md:row-span-2", info: "Notes to cards to quizzes all in 1 place for those late night revision sessions" },
   ];
