77d4f1fae47c347cdf6bdbdd7c9995c8600ce1d7
vertexed25-byte <vertexed.25@gmail.com>
2025-09-11 16:33:46 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -55,7 +55,7 @@ function App() {
               <Route path="study-zone" element={<StudyZone />} />
               <Route path="chatbot" element={<AIChatbot />} />
               <Route path="planner" element={<StudyPlanner />} />
-              <Route path="image-answer" element={<ImageAnswer />} />
+              <Route path="Answer-Reviewer" element={<AnswerReviewer />} />
               <Route path="paper-maker" element={<PaperMaker />} />
               <Route path="about" element={<About />} />
               <Route path="settings" element={<UserSettings />} />
