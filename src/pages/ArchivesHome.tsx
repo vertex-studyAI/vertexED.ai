dbae647acdeb7764127ba776cf03cfc388d1616a
vertexed25-byte <vertexed.25@gmail.com>
2025-10-18 14:04:55 +0530

    Update ArchivesHome.tsx

--- src/pages/ArchivesHome.tsx
+++ src/pages/ArchivesHome.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
