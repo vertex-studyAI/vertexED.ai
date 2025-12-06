58a6350eecaa5dba0870611cf5883e1587e2bf36
vertexed25-byte <vertexed.25@gmail.com>
2025-12-06 12:03:46 +0530

    Update AIChatbot.tsx

--- src/pages/AIChatbot.tsx
+++ src/pages/AIChatbot.tsx
@@ -17,6 +17,7 @@ import ReactMarkdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import "@/styles/custom.css";
 import { fetchChatbotAnswer } from "@/lib/chatbotApi"; // expected to exist in your project
 
 // -----------------------------------------------------------------------------
