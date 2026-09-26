fea275d9e67038011936be948f87067a5979a3ed
build-the-future-11 <ryangomez.hs@gmail.com>
2026-09-26 15:36:09 +0530

    ops: centralize public intake routes

---
+++ src/lib/intakeRoutes.mjs
@@ -0,0 +1,6 @@
+export const PUBLIC_INTAKE_ROUTES = Object.freeze({
+  support: "https://tally.so/r/XxXBpj?utm_source=footer",
+  privacyRequest: "https://tally.so/r/b5P4E1?utm_source=footer",
+  security: "https://tally.so/r/A7ZbOe?utm_source=footer",
+  schoolPilot: "https://tally.so/r/WOypkk?utm_source=footer",
+});
